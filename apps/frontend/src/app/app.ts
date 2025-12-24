5be4d8af9485cdeef6db6415442df2696c1c501d
abhishekg <abhishekg@backbase.com>
2025-12-24 12:51:14 +0530

    feat:(add bootstrap and ng-bootstrap)

--- apps/frontend/src/app/app.ts
+++ apps/frontend/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  imports: [RouterModule],
+  imports: [RouterModule, NgbModule],
   selector: 'app-root',
   templateUrl: './app.html',
-  styleUrl: './app.css',
+  styleUrl: './app.scss',
 })
 export class App {
   protected title = 'frontend';
